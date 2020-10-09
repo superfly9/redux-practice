fc5c14490dd95a7e9cd19647b0cf19687834a913
unknown <superfly09@daum.net>
2020-10-09 14:47:28 +0900

    saga-reducer추가

--- src/saga/userTicket.js
+++ src/saga/userTicket.js
@@ -5,15 +5,14 @@ import {
     GET_USER_TICKET_FAILURE} from '../constants/actionTypes';
 import axios from 'axios';
 
-function getUserTicketApi (param) {
+function getUserTicketApi (params) {
     let url = '/api/userTicket'
-    return axios.get(url,param);
-}
+    return axios.get(url,params);
+} // promise를 리턴
 function* getUserTicket (action) {
     try {
         //서버 요청시 call
-        const result = yield call(getUserTicketApi,action.param);
-
+        const result = yield call(getUserTicketApi,action.params);
         //api요청으로 받아온 데이터를 액션 객체에 넣어서 reducer에 dispatch해줌
         yield put({type:GET_USER_TICKET_SUCCESS,data:result.data});
     } catch (e) {
